c3e76570f25bcfdca00515fd78d07b2f39957e45
Yewen Li <liyewen521@gmail.com>
2023-10-10 11:27:31 +0800

    更新简单的vim配置 + 找到了便捷式的tmux

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Install oh-my-tmux
+# Install oh-my-tmux 
 apt install -y tmux
+
+# or use portable tmux
+curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest | grep "browser_download_url.*appimage" | cut -d : -f 2,3 | tr -d \" | wget -qi - && chmod +x tmux.appimage
+alias tmux="$HOME/tmux.appimage"
+
+# Use tmux configuration
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
